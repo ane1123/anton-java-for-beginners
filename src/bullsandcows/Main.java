10969004be2c0563a3bc799f0dc9b611aace9d17
ane1123 <92170135+ane1123@users.noreply.github.com>
2021-12-02 20:48:08 +0500

    task bull and cows 4

--- src/bullsandcows/Main.java
+++ src/bullsandcows/Main.java
@@ -1,7 +1,34 @@
 package bullsandcows;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bulls and Cows");
+        Random r = new Random();
+
+        String alphabet = "abcdef";
+
+      char latter = (alphabet.charAt(r.nextInt(alphabet.length())));
+      int a = (int) ((Math.random() * ((9))) + 0);
+      int s  = (int) ((Math.random() * ((9))) + 0);
+      int c = (int) ((Math.random() * ((9))) + 0);
+      String num1 = Integer.toString(a);
+      String num2 = Integer.toString(s);
+      String num3 = Integer.toString(c);
+
+
+
+      int u = (int) ((Math.random() * ((4))) + 1);
+
+
+      if(u == 1){
+          System.out.println(latter + num1 + num2 + num3 );
+      }if (u == 2){
+            System.out.println( num1 + latter+ num2 + num3 );
+        }if (u == 3){
+            System.out.println( num1 + num2 + latter + num3 );
+        }if (u == 4){
+            System.out.println( num1 +  num2 + num3 + latter );
+        }
     }
 }
